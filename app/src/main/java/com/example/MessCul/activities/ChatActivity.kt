7db848a2885ad871a932677ba05990f2729db719
CaptainSandpiper <mr.rayds@mail.ru>
2019-05-05 21:35:04 +0300

    chat activity added

--- app/src/main/java/com/example/MessCul/activities/ChatActivity.kt
+++ app/src/main/java/com/example/MessCul/activities/ChatActivity.kt
@@ -2,12 +2,162 @@ package com.example.MessCul.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.MessCul.R
+import com.example.MessCul.models.FriendlyMessage
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+
+    var userId:String? = null;
+    var mFirebaseDatabaseRef:DatabaseReference? = null;
+    var mFirebaseUser: FirebaseUser? = null;
+
+    var mLinearLayoutManager:LinearLayoutManager? = null;
+    var mFirebaseAdapter:FirebaseRecyclerAdapter<FriendlyMessage, MessageViewHolder>? = null; //back to create MessageViewHolder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        mFirebaseUser = FirebaseAuth.getInstance().currentUser;
+
+        userId = intent.extras.getString("userId");
+        mLinearLayoutManager = LinearLayoutManager(this);
+        mLinearLayoutManager!!.stackFromEnd = true;
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true);
+
+        mFirebaseDatabaseRef = FirebaseDatabase.getInstance().reference;
+        mFirebaseAdapter = object: FirebaseRecyclerAdapter<FriendlyMessage, MessageViewHolder>(FriendlyMessage::class.java,
+                                                                                                R.layout.item_message,
+                                                                                                MessageViewHolder::class.java,
+                                                                                                mFirebaseDatabaseRef!!.child("messages"))
+        {
+            override fun populateViewHolder(viewHolder: MessageViewHolder?, friendlyMessage: FriendlyMessage?, position: Int)
+            {
+                if(friendlyMessage!!.text !=  null)
+                {
+                    viewHolder!!.bindView(friendlyMessage)
+
+                    var currentUserId = mFirebaseUser!!.uid;
+                    var isMe: Boolean  = friendlyMessage!!.id!!.equals(currentUserId);
+                    if(isMe)
+                    {
+                        //Me to right side
+                        viewHolder.profileImageViewRight!!.visibility = View.VISIBLE;
+                        viewHolder.profileImageView!!.visibility = View.GONE;
+                        viewHolder.messageTextView!!.gravity = (Gravity.CENTER_VERTICAL or Gravity.RIGHT);
+                        viewHolder.messengerTextView!!.gravity = (Gravity.CENTER_VERTICAL or Gravity.RIGHT);
+
+                        //get image url for me
+                        mFirebaseDatabaseRef!!.child("Users")
+                            .child(currentUserId)
+                            .addValueEventListener(object:ValueEventListener{
+                                override fun onDataChange(data: DataSnapshot)
+                                {
+                                    var imageUrl = data!!.child("thumb_image").value.toString();
+                                    var displayName = data!!.child("display_name").value.toString();
+
+//                                    viewHolder.messengerTextView!!.text = displayName.toString();
+                                    viewHolder.messengerTextView!!.text = "I wrote...";
+
+                                    Picasso.get()
+                                        .load(imageUrl)
+                                        .placeholder(R.drawable.profile_img)
+                                        .into(viewHolder.profileImageViewRight);
+                                }
+
+                                override fun onCancelled(p0: DatabaseError)
+                                {
+
+                                }
+                            })
+                    }
+                    else
+                    {
+                        //to other person show image view in left side
+
+                        //Me to right side
+                        viewHolder.profileImageViewRight!!.visibility = View.GONE;
+                        viewHolder.profileImageView!!.visibility = View.VISIBLE;
+                        viewHolder.messageTextView!!.gravity = (Gravity.CENTER_VERTICAL or Gravity.LEFT);
+                        viewHolder.messengerTextView!!.gravity = (Gravity.CENTER_VERTICAL or Gravity.LEFT);
+
+                        //get image url for me
+                        mFirebaseDatabaseRef!!.child("Users")
+                            .child(userId.toString())
+                            .addValueEventListener(object:ValueEventListener{
+                                override fun onDataChange(data: DataSnapshot)
+                                {
+                                    var imageUrl = data!!.child("thumb_image").value.toString();
+                                    var displayName = data!!.child("display_name").value.toString();
+
+                                    viewHolder.messengerTextView!!.text = "$displayName wrote...";
+
+                                    Picasso.get()
+                                        .load(imageUrl)
+                                        .placeholder(R.drawable.profile_img)
+                                        .into(viewHolder.profileImageView);
+                                }
+
+                                override fun onCancelled(p0: DatabaseError)
+                                {
+
+                                }
+                            })
+                    }
+                }
+            }
+        }
+
+        //set the RecyclerView
+        messageRecyclerView.layoutManager = mLinearLayoutManager;
+        messageRecyclerView.adapter = mFirebaseAdapter;
+
+        sendButton.setOnClickListener{
+            if(!intent.extras.get("name").toString().equals(""))
+            {
+                var currentUserName = intent.extras.get("name");
+                var mCurrentUserId = mFirebaseUser!!.uid;
+
+                var friendlyMessage =  FriendlyMessage(mCurrentUserId, messageEdt.text.toString().trim(), currentUserName.toString().trim())
+
+                mFirebaseDatabaseRef!!.child("messages")
+                    .push().setValue(friendlyMessage); // push used because every message must have ow unique id
+
+                messageEdt.setText("");
+            }
+        }
+    }
+
+    class MessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        var messageTextView: TextView? = null;
+        var messengerTextView:TextView? = null;
+        var profileImageView: CircleImageView? = null;
+        var profileImageViewRight: CircleImageView? = null;
+
+        fun bindView(friendlyMessage: FriendlyMessage)
+        {
+            messageTextView = itemView.findViewById(R.id.messageTextView);
+            messengerTextView = itemView.findViewById(R.id.messengerTextView);
+            profileImageView = itemView.findViewById(R.id.messengerImageView);
+            profileImageViewRight = itemView.findViewById(R.id.messengerImageViewRigth);
+
+            messengerTextView!!.text = friendlyMessage.name;
+            messageTextView!!.text  = friendlyMessage.text;
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/MessCul/models/FriendlyMessage.kt
@@ -0,0 +1,15 @@
+package com.example.MessCul.models
+
+class FriendlyMessage() {
+    var id:String? = null;
+    var text:String? = null;
+    var name:String? = null;
+
+    constructor(id:String, text: String, name: String ):this()
+    {
+        this.id = id;
+        this.text = text;
+        this.name = name;
+
+    }
+}
